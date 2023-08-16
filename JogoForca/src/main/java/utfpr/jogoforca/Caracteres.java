b3e9a929863a6c65b547a23a73f337075cacb0a0
Fabio Kinukawa <81826645+fack07@users.noreply.github.com>
2023-08-16 20:37:48 -0300

    Update Caracteres.java

--- JogoForca/src/main/java/utfpr/jogoforca/Caracteres.java
+++ JogoForca/src/main/java/utfpr/jogoforca/Caracteres.java
@@ -4,24 +4,22 @@ package utfpr.jogoforca;
 
 public class Caracteres {
     
-    public char[] conversorCaracteres(String palavra){
+    public StringBuilder conversorCaracteres(String palavra){
         char[] tracos = palavra.toCharArray();
-        
+        StringBuilder palavraEscondida = new StringBuilder();
         for (char c : tracos) {
             System.out.print("_ ");
+            palavraEscondida.append("_");
         }
-        return tracos;
+        return palavraEscondida;
     }
     
-    public StringBuilder acertoDeCaracteres(String palavra, char d){
+    public StringBuilder acertoDeCaracteres(String palavra, StringBuilder palavraEscondida, char d){
         char[] espacos = palavra.toCharArray();
-        
-        StringBuilder palavraEscondida = new StringBuilder();
 
-      
-        
+        //System.out.println(palavraEscondida);
             for (int c = 0; c < palavra.length(); c++) {
-                palavraEscondida.append("_");
+                    
                 if (d == espacos[c]) {
                     palavraEscondida.setCharAt(c, espacos [c]);
                     
