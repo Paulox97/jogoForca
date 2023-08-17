373994e0c1e075ed4755fb5a835c8eaace35ccb9
Paulo Amaral <57416425+Paulox97@users.noreply.github.com>
2023-08-17 20:03:36 -0300

    Update JogoForca.java
    Adicionado o desconto da vida, da linha 62 a 71

--- JogoForca/src/main/java/utfpr/jogoforca/JogoForca.java
+++ JogoForca/src/main/java/utfpr/jogoforca/JogoForca.java
@@ -13,12 +13,16 @@ public class JogoForca {
         Dificuldade dificuldade = new Dificuldade();
         LetrasJogador letraEscolhida = new LetrasJogador();
         Caracteres caracteres = new Caracteres();
-        StringBuilder palavraEscondida = new StringBuilder();
+        StringBuilder palavraAux = new StringBuilder();
         StringBuilder palavraEscolhida = new StringBuilder();
+        Set<Character> letrasDigitadas = new HashSet<>();
         
         int escolhaJogador;
+        int vidaJogador = 6;
         String aux;
         char letra;
+        
+        
         
         
         
@@ -40,17 +44,38 @@ public class JogoForca {
                 
         while (palavraDescoberta == false){
             letra = letraEscolhida.LetraEscolhida();
+
+            if(letrasDigitadas.contains(letra)){ //verifica se a letra digitada já foi digitada antes
+                System.out.println("Letra já digitada");
+                
+                continue;
+            }
             
-            //caracteres.acertoDeCaracteres(aux, letra);
-           palavraEscolhida = caracteres.acertoDeCaracteres(aux, palavraEscolhida, letra);
-           System.out.println(palavraEscolhida);
+
+            letrasDigitadas.add(letra); //adiciona a letra digitada ao histórico de letras
+            
+            //Dar um jeito de fazer essa vida funcionar;
+
+            
+            palavraEscolhida = caracteres.acertoDeCaracteres(aux, palavraEscolhida, letra);
+            
+            int cont = 0; //Inicio do contador que irá auxiliar se o usuário acertou alguma letra
+            for (int c = 0; c < aux.length(); c++){
+                if (letra != aux.charAt(c)){
+                    cont++; //Aqui pego o valor de quantas vezes a letra digitada existe dentro da palavra sorteada
+                }
+                if (cont == aux.length()){ //Caso a quantidade do contador for igual ao tamanho da palavra sorteada significa que o usuário errou, então descontamos uma vida
+                        vidaJogador--;
+                        System.out.println("Você perdeu uma vida \nVida atual: " + vidaJogador);
+                }
+            }
+            
+            System.out.println(palavraEscolhida);
+            System.out.println("Letras digitadas: " + letrasDigitadas);
         }
-        
-// Metodo para salvar a letra digitada
-//Set<Character> letrasDigitadas = new HashSet<>();
-        
-        
-       
-           
+//Vida -- feito
+//Completar a palavra
+//Boneco
+
     }
 }
