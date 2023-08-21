1a0cf2e4bf518fb91fe932192ae05a6323fbb692
Fabio Kinukawa <81826645+fack07@users.noreply.github.com>
2023-08-20 22:19:42 -0300

    Update JogoForca.java

--- JogoForca/src/main/java/utfpr/jogoforca/JogoForca.java
+++ JogoForca/src/main/java/utfpr/jogoforca/JogoForca.java
@@ -22,39 +22,39 @@ public class JogoForca {
         String aux;
         char letra;
         
-        
+        //Seleção de dificuldade do jogo
         escolhaJogador = dificuldade.escolhaDificuldade();
         
         System.out.println("---------------------------------------");
         System.out.println("           INICIANDO O JOGO");
         System.out.println("---------------------------------------");
         
+        //Sortea a palavra de acordo com a dificuldade escolhida pelo jogado e transfoma em minúscula
         aux = palavra.sorteador(escolhaJogador).toLowerCase();
         
         System.out.println(aux);
-        
+
+        //prepara a variável de resposta com "_" do tamanho da palavra escolhida
         palavraEscolhida = caracteres.conversorCaracteres(aux);
         
         boolean palavraDescoberta = false;
         
-                
+        //Looping principal enquanto o jogo não acabar       
         while (palavraDescoberta == false){
+            //recebe a letra digitada pelo jogador e converte para minúscula
             letra = Character.toLowerCase(letraEscolhida.LetraEscolhida());
-
-            if(letrasDigitadas.contains(letra)){ //verifica se a letra digitada já foi digitada antes
+            //verifica se a letra digitada já foi digitada antes
+            if(letrasDigitadas.contains(letra)){ 
                 System.out.println("Letra já digitada");
-                
                 continue;
             }
-            
-
-            letrasDigitadas.add(letra); //adiciona a letra digitada ao histórico de letras
-
-            
+            //adiciona a letra digitada ao histórico de letras
+            letrasDigitadas.add(letra); 
+            //verifica se a palavra contém a letra digitada
             palavraEscolhida = caracteres.acertoDeCaracteres(aux, palavraEscolhida, letra);
             
             qtdVidas = vidaJogador.vidasDoJogador(aux, letra, qtdVidas);
-            
+            //Se acabar as vidas, o jogo termina
             if (qtdVidas == 0){
                 System.out.println("QUE PENA, VOCÊ PERDEU O JOGO");
                 //System.out.println("-------------------------------------------------");
@@ -62,12 +62,13 @@ public class JogoForca {
                 
                 break;
             }
-            
+            //Mostra a situação da respota e as letras digitadas
             System.out.println("-------------------------------------------------");
             System.out.println(palavraEscolhida);
             System.out.println("-------------------------------------------------");
             System.out.println("LETRAS DIGITADAS: " + letrasDigitadas);
-            
+          
+            //Se a variável de resposta for igual a palavra escolhida, o jogo termina
             if (palavraEscolhida.toString().equals(aux)){
                 System.out.println("-------------------------------------------------");
                 System.out.println("PARABÉNS VOCÊ VENCEU O JOGO!");
