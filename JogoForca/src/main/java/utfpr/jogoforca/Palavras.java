dab0eebc5a7114c5e0e7844b64cdd324e9c9cc6d
Fabio Kinukawa <81826645+fack07@users.noreply.github.com>
2023-08-20 22:26:47 -0300

    Update Palavras.java

--- JogoForca/src/main/java/utfpr/jogoforca/Palavras.java
+++ JogoForca/src/main/java/utfpr/jogoforca/Palavras.java
@@ -6,22 +6,32 @@ import java.util.Random;
 
 
 public class Palavras {
+    //Biblioteca de palavras do jogo, separado por dificuldade
     Random gerador = new Random();
     
     public String sorteador(int c){
         if (c == 1){
+            //Array de palavras
             String palavrasFaceis[] = {"baixo", "caixa", "disco", "foice", "harpa"};
-            String escolha = palavrasFaceis[gerador.nextInt(0, 4)];
+            //Seleciona uma palavra aleatória da biblioteca
+            String escolha = palavrasFaceis[gerador.nextInt(0, 4)]; 
+            //retorna a palavra
             return escolha;
         }
         else if (c == 2){
+            //Array de palavras
             String palavrasMedias[] = {"abafador", "baioneta", "carretel", "fantasia", "molinete"};
+            //Seleciona uma palavra aleatória da biblioteca
             String escolha = palavrasMedias[gerador.nextInt(0, 4)];
+            //retorna a palavra
             return escolha;   
         }
         else {
+            //Array de palavras
             String palavrasDificil[] = {"coqueteleira", "guarda-chuva", "metralhadora", "tornozeleira", "fita-isolante"};
+            //Seleciona uma palavra aleatória da biblioteca
             String escolha = palavrasDificil[gerador.nextInt(0, 4)];
+            //retorna a palavra
             return escolha;
         }
     }
